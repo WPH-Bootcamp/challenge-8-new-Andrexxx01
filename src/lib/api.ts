37687b0d6e5d0de28d435a61d36170df8daebaf5
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-01-18 12:11:25 +0700

    First Commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,3 +13,35 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+export const getMovieById = async (id: number) => {
+  const res = await api.get(`/movie/${id}`);
+  return res.data;
+};
+
+export const getMovieCredits = async (id: number) => {
+  const res = await api.get(`/movie/${id}/credits`);
+  return res.data;
+};
+
+export const getTrendingMovies = async (page = 1) => {
+  const res = await api.get("/trending/movie/week", {
+    params: { page },
+  });
+  return res.data;
+};
+
+export const getNewReleases = async (page: number) => {
+  const res = await api.get("/movie/now_playing", {
+    params: {
+      page,
+    },
+  });
+
+  return res.data;
+};
+
+export const getMovieVideos = async (id: number) => {
+  const res = await api.get(`/movie/${id}/videos`);
+  return res.data;
+};
